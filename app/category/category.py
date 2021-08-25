f77ca1c0da4b652bce894eff584c61de0a2dcfa6
JhonathaNunes <jhonathasilveira@gmail.com>
2021-08-25 20:11:35 -0300

    Crud categories

--- app/category/category.py
+++ app/category/category.py
@@ -5,6 +5,8 @@ category_bp = Blueprint(
     "category", __name__, template_folder="templates", static_folder="static"
 )
 
+columns = ["Nome", "Descrição", "Visualizar"]
+
 
 @category_bp.route("/admin/category/", methods=["GET"])
 def index():
@@ -23,7 +25,9 @@ def index():
 def category_form():
     return render_template(
         "category_form.j2",
-        title="Nova categoria"
+        title="Nova categoria",
+        action="/admin/category",
+        category=None
     )
 
 
@@ -39,9 +43,66 @@ def create_category():
     db.session.add(category)
     db.session.commit()
 
+    categories = Category.query.all()
+
     return render_template(
         "category_listing.j2",
         title="Categorias",
         path_new="/admin/category/new",
-        message="Categoria cadastrada com sucesso"
+        message="Categoria cadastrada com sucesso",
+        columns=columns,
+        data=categories
+    )
+
+
+@category_bp.route("/admin/category/<int:id>", methods=["GET"])
+def category_view(id: int):
+    category = Category.query.get(id)
+
+    return render_template(
+        "category_form.j2",
+        title="Editar categoria",
+        category=category,
+        action=f"/admin/category/{id}"
+    )
+
+
+@category_bp.route("/admin/category/<int:id>", methods=["DELETE"])
+def delete_category(id: int):
+    category = Category.query.get(id)
+
+    db.session.delete(category)
+    db.session.commit()
+
+    categories = Category.query.all()
+
+    return render_template(
+        "category_listing.j2",
+        title="Categorias",
+        path_new="/admin/category/new",
+        columns=columns,
+        data=categories,
+        message="Categoria exluída"
+    )
+
+
+@category_bp.route("/admin/category/<int:id>", methods=["POST"])
+def update_category(id: int):
+    form = request.form
+
+    category = Category.query.get(id)
+
+    category.name = form["name"]
+    category.description = form["description"]
+    db.session.commit()
+
+    categories = Category.query.all()
+
+    return render_template(
+        "category_listing.j2",
+        title="Categorias",
+        path_new="/admin/category/new",
+        columns=columns,
+        data=categories,
+        message="Categoria atualizada"
     )
