fb60cf2200c3cf3da6e71a9529ddfb20719031ce
JhonathaNunes <jhonathasilveira@gmail.com>
2021-10-28 22:51:47 -0300

    Fix call

--- app/login/login.py
+++ app/login/login.py
@@ -31,7 +31,7 @@ def login():
         params=payload
     )
 
-    if response.status_code == 200 and not response.json()['success']:
+    if not response.json()['success']:
         flash('Preencha o reCaptcha')
         return redirect(url_for('login.render_login'))
 
