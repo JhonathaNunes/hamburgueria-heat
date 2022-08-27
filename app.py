cf3978017d34ae8d09078af78ac80b846a4c5a8b
JhonathaNunes <jhonathasilveira@gmail.com>
2022-08-27 14:50:06 -0300

    Another test

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import os
 from app import init_app
+
 app = init_app()
 
-'''
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0')
-'''
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
